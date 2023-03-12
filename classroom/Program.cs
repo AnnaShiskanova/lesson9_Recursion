01ccaa6066fa95dce5e2c42acba618329f0e1f26
Anna <shiskanovaag@mail.ru>
2023-03-12 19:00:13 +1000

    add task 65

---
+++ classroom/Program.cs
@@ -0,0 +1,26 @@
+﻿static void Task65()
+{
+/*Задача 65: Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 5, 6, 7, 8"*/
+
+
+int PutNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+string ReturnNumber(int m, int n)
+{
+    if (m == n)
+    return n + " ";  
+    else 
+    return m + " " + ReturnNumber(m + 1, n);
+}
+ReturnNumber(4, 8);
+System.Console.WriteLine(ReturnNumber(PutNumber("Введите m:  "), PutNumber("Введите n: ")));
+}
+Task65();
